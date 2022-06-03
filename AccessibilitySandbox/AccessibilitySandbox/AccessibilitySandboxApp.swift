07d5473506e2ec8e997491b9577b008eff635705
Tal Spektor <tal.s@zemingo.com>
2022-06-03 22:57:41 +0300

    accessibility

---
+++ AccessibilitySandbox/AccessibilitySandbox/AccessibilitySandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilitySandboxApp.swift
+//  AccessibilitySandbox
+//
+//  Created by Tal talspektor on 03/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct AccessibilitySandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
