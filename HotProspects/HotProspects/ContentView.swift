b44bb80f6f20416d481b126fa5ac738828bcb75d
Tal Spektor <tal.s@zemingo.com>
2022-06-05 19:45:33 +0300

    Hot Prospects

---
+++ HotProspects/HotProspects/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HotProspects
+//
+//  Created by Tal talspektor on 03/06/2022.
+//
+
+import SwiftUI
+import UserNotifications
+
+
+struct ContentView: View {
+    @State private var backgroundColor = Color.red
+
+    var body: some View {
+        Text("")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HotProspects/HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Tal talspektor on 03/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
