793d57e57c44adc5af456b8fe53481f8eb5c70cb
Tal Spektor <tal.s@zemingo.com>
2022-06-17 23:48:19 +0300

    Haptic

---
+++ Flashzilla/Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Tal talspektor on 10/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
