f2cdc62e9cfbb119efe1a816a8959abd73ef40c2
Tal Spektor <tal.s@zemingo.com>
2023-11-10 23:39:33 +0200

    Trakr initial

---
+++ Trakr/Trakr/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Trakr
+//
+//  Created by Tal talspektor on 10/11/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Trakr/Trakr/TrakrApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrakrApp.swift
+//  Trakr
+//
+//  Created by Tal talspektor on 10/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct TrakrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
